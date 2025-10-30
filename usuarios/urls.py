415bceed870ce2564034d859a2170c39eab59a81
JeroRodriguez <jeronimoroseag@gmail.com>
2025-10-30 10:48:51 -0500

    feat(auth): incorporar path vista de perfil de usuarios registrados en urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-from usuarios.views.auth_views import RegistroView
+from usuarios.views.auth_views import RegistroView, PerfilView
 
 urlpatterns = [
     # Registro público
-     path('auth/register/', RegistroView.as_view(), name='registro')
+     path('auth/register/', RegistroView.as_view(), name='registro'),
+
+     # Perfil del usuario autenticado
+    path('perfil/', PerfilView.as_view(), name='perfil')
 ]
