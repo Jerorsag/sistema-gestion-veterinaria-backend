441b5b68d4e11577d8ee773e5b6fa4269688c5ea
JeroRodriguez <jeronimoroseag@gmail.com>
2025-10-31 22:15:09 -0500

    feat(auth): implementar views para login JWT

--- usuarios/views/auth_views.py
+++ usuarios/views/auth_views.py
@@ -1,8 +1,23 @@
 from rest_framework import status, generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
-from usuarios.serializers.auth_serializer import RegistroSerializer, UsuarioPerfilSerializer
+from usuarios.serializers.auth_serializer import RegistroSerializer, UsuarioPerfilSerializer, CustomTokenObtainPairSerializer
+
+# Jeronimo Rodriguez 10/31/2025 
+class CustomTokenObtainPairView(TokenObtainPairView):
+    """
+    Vista personalizada para la obtención de tokens JWT (Login).
+
+    - Endpoint: POST /api/v1/auth/login/
+    - Permite autenticar al usuario y retornar:
+        • access token
+        • refresh token
+        • información básica del usuario autenticado
+    - Utiliza el serializer CustomTokenObtainPairSerializer.
+    """
+    serializer_class = CustomTokenObtainPairSerializer
 
 # Jeronimo Rodriguez 10/30/2025 
 class RegistroView(generics.CreateAPIView):
