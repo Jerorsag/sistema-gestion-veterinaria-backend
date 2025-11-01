a7f446b1a844ef0210539a03f0cb3257e3db4fb8
JeroRodriguez <jeronimoroseag@gmail.com>
2025-10-31 22:15:31 -0500

    feat(auth): implementar path a URL para login JWT

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
-from usuarios.views.auth_views import RegistroView, PerfilView
+from usuarios.views.auth_views import (
+    CustomTokenObtainPairView, 
+    RegistroView, 
+    PerfilView
+)
 
 urlpatterns = [
+    # Autenticación JWT
+    path('auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     # Registro público
      path('auth/register/', RegistroView.as_view(), name='registro'),
 
