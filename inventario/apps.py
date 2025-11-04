a464be9391a54729b032ce5bdf68efea2c8ea1e1
Luisa <mlondono408@cue.edu.co>
2025-11-03 20:48:23 -0500

    Refactor: modificando ubicación de inventario

--- inventario/apps.py
+++ inventario/apps.py
@@ -2,8 +2,8 @@ from django.apps import AppConfig
 
 class InventarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clinica_veterinaria.inventario'
+    name = 'inventario'
 
     def ready(self):
         # Importa las señales para activar el patrón Observer
-        import clinica_veterinaria.inventario.signals
+        import inventario.signals
