6667c309bce4f74665405f9244ada2b77cd25016
Xiomara Ocampo <xocampo_165@cue.edu.co>
2025-11-04 09:42:55 -0500

    avance base de citas

---
+++ citas/services/accionescita.py
@@ -0,0 +1,142 @@
+from datetime import datetime
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError, PermissionDenied
+from citas.services.disponibilidad import obtener_horarios_disponibles
+from citas.services.notificacion import notificar_observadores
+from ..models import Cita, Servicio
+from ..models.Choices import EstadoCita
+from usuarios.models import Usuario
+from mascotas.models import Mascota
+
+
+def agendar_nueva_cita(data: dict, usuario: Usuario) -> Cita:
+    """
+    Servicio (Patrón Command) para crear una nueva cita (RF-004).
+    se encarga de Orquestar la validación, creación 
+    y notificación de una nueva cita.
+    """
+    mascota_id = data.get('mascota_id')
+    veterinario_id = data.get('veterinario_id')
+    servicio_id = data.get('servicio_id')
+    fecha_hora_str = data.get('fecha_hora')
+
+    # --- 1. Validación de Lógica de Negocio ---
+    try:
+        mascota = Mascota.objects.get(id=mascota_id)
+         # Usamos el modelo Usuario porque veterinario es un Usuario con un rol específico
+        veterinario = Usuario.objects.get(id=veterinario_id) 
+        servicio = Servicio.objects.get(id=servicio_id)
+    except (Mascota.DoesNotExist, Usuario.DoesNotExist, Servicio.DoesNotExist):
+        raise ValidationError("La mascota, veterinario o servicio no existen.")
+    
+    # Verificación de permisos: El cliente solo puede agendar para sus mascotas
+    # Verificamos si el usuario tiene el rol 'cliente'
+
+    if 'cliente' in [r.rol.nombre for r in usuario.usuario_roles.all()]:
+        # Si es cliente, verificamos que la mascota le pertenezca
+        if mascota.cliente.usuario != usuario:
+            raise PermissionDenied("No tienes permiso para agendar citas para esta mascota.")
+        
+
+    # Validación de Disponibilidad (CP-021)
+    # Convertimos el texto de la fecha (ej. "2025-11-20T14:00:00Z") a un objeto datetime
+    fecha_hora = timezone.make_aware(datetime.fromisoformat(fecha_hora_str.rstrip("Z")))
+
+    # ¡REUTILIZAMOS el servicio de disponibilidad!
+    horarios_libres = obtener_horarios_disponibles(veterinario.id, fecha_hora.date())
+
+    if fecha_hora.strftime("%H:%M") not in horarios_libres:
+        raise ValidationError("Conflicto de Horario: El veterinario no está disponible a esta hora.")
+
+    # --- 2. Ejecución (Creación) ---
+    cita = Cita.objects.create(
+        mascota=mascota,
+        veterinario=veterinario,
+        servicio=servicio,
+        fecha_hora=fecha_hora,
+        observaciones=data.get('observaciones', ''),
+       estado=EstadoCita.AGENDADA
+    )
+
+    # --- 3. Post-Acción (Llamada al servicio de notificación) ---
+    notificar_observadores(evento="CITA_CREADA", cita=cita)
+
+    return cita
+
+
+def cancelar_cita(cita_id: str, usuario: Usuario) -> Cita:
+    """
+    Servicio (Patrón Command) para cancelar una cita (RF-007).
+    Implementa Patrón State (Agendada -> Cancelada).
+    Implementa Patrón Observer (Notifica cancelación).
+
+    RESPONSABILIDAD: Orquestar la validación, cambio de estado (cancelación)
+    y notificación de una cita.
+    """
+    try:
+        cita = Cita.objects.get(id=cita_id)
+    except Cita.DoesNotExist:
+        raise ValidationError("La cita no existe.")
+
+    # Verificación de permisos (RF-007):
+    es_propietario = cita.mascota.cliente.usuario == usuario
+    roles_usuario = [r.rol.nombre for r in usuario.usuario_roles.all()]
+    es_rol_administrativo = any(r in ['recepcionista', 'administrador', 'veterinario'] for r in roles_usuario)
+
+    if not es_propietario and not es_rol_administrativo:
+        raise PermissionDenied("No tienes permiso para cancelar esta cita.")
+
+    # Lógica del Patrón State
+    if cita.estado == EstadoCita.CANCELADA:
+        raise ValidationError("Esta cita ya ha sido cancelada.")
+    if cita.estado == EstadoCita.COMPLETADA:
+        raise ValidationError("No se puede cancelar una cita ya completada.")
+
+    cita.estado  = EstadoCita.CANCELADA
+    cita.save()
+
+    # Llamada al servicio de notificación
+    notificar_observadores(evento="CITA_CANCELADA", cita=cita)
+
+    return cita
+
+
+def reagendar_cita(cita_id: str, nueva_fecha_hora_str: str, usuario: Usuario) -> Cita:
+    """
+    Servicio (Patrón Command) para reagendar una cita (RF-006).
+    
+    RESPONSABILIDAD: Orquestar la validación, cambio de fecha,
+    y notificación de una cita.
+    """
+    try:
+        cita = Cita.objects.get(id=cita_id)
+    except Cita.DoesNotExist:
+        raise ValidationError("La cita no existe.")
+
+    # Verificación de permisos
+    es_propietario = cita.mascota.cliente.usuario == usuario
+    roles_usuario = [r.rol.nombre for r in usuario.usuario_roles.all()]
+    es_rol_administrativo = any(r in ['recepcionista', 'administrador'] for r in roles_usuario)
+
+    if not es_propietario and not es_rol_administrativo:
+        raise PermissionDenied("No tienes permiso para reagendar esta cita.")
+
+    # --- Lógica de Negocio ---
+    # 1. Validar la nueva hora
+    nueva_fecha_hora = timezone.make_aware(datetime.fromisoformat(nueva_fecha_hora_str.rstrip("Z")))
+
+    # ¡REUTILIZAMOS el servicio de disponibilidad!
+    horarios_libres = obtener_horarios_disponibles(cita.veterinario.id, nueva_fecha_hora.date())
+
+    if nueva_fecha_hora.strftime("%H:%M") not in horarios_libres:
+        raise ValidationError("Conflicto de Horario: El veterinario no está disponible a esta hora.")
+
+    # 2. Actualizar
+    cita.fecha_hora = nueva_fecha_hora
+    cita.estado = EstadoCita.AGENDADA # Reactivamos si estaba cancelada
+    cita.save()
+
+    # 3. Notificar
+    notificar_observadores(evento="CITA_REAGENDADA", cita=cita)
+
+    return cita
--- clinica_veterinaria/settings.py
+++ clinica_veterinaria/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'usuarios',
     'mascotas',
     'consultas',
+    'inventario',
     'citas',
 ]
 
