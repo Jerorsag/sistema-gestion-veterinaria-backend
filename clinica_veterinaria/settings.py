e0a3ad7ff09af992d320e36a913e99137e9e81a5
sarasanchez07 <estradasanchezsara2007@gmail.com>
2025-11-07 00:10:45 -0500

    chore(settings): ajustes generales en configuración del proyecto

--- clinica_veterinaria/settings.py
+++ clinica_veterinaria/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+
     
     # local apps
     'usuarios',
@@ -157,6 +158,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,
 }
 
 SIMPLE_JWT = {
