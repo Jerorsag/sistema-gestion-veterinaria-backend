c499c465727acd446daa583297e2658d6081e566
Xiomara Ocampo <xocampo_165@cue.edu.co>
2025-11-15 21:39:27 -0500

    correccion de compatibilidad

--- citas/signals.py
+++ citas/signals.py
@@ -4,10 +4,6 @@ import django.dispatch
 Define las señales personalizadas (los "eventos" o "gritos")
 que la app 'citas' enviará.
 """
-
-# Define un proveedor de argumentos que enviará 'cita'
-cita_signal_args = ["cita"]
-
-cita_agendada_signal = django.dispatch.Signal(providing_args=cita_signal_args)
-cita_cancelada_signal = django.dispatch.Signal(providing_args=cita_signal_args)
-cita_reagendada_signal = django.dispatch.Signal(providing_args=cita_signal_args)
+cita_agendada_signal = django.dispatch.Signal()
+cita_cancelada_signal = django.dispatch.Signal()
+cita_reagendada_signal = django.dispatch.Signal()
