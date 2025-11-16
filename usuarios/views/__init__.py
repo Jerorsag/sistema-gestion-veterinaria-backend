9595b3e112a3a6cbff72b9127edb1ed7889be639
JeroRodriguez <jeronimoroseag@gmail.com>
2025-11-16 18:35:24 -0500

    Se eliminan los archivos de views sin modularizar

---
+++ usuarios/views/__init__.py
@@ -0,0 +1,28 @@
+from usuarios.views.auth import (
+    CustomTokenObtainPairView,
+    RegistroView,
+    PerfilView,
+    cambiar_password_view,
+    logout_view,
+    verificar_token_view,
+    ResetPasswordRequestView,
+    ResetPasswordConfirmView,
+)
+from usuarios.views.crud import UsuarioViewSet
+from usuarios.views.profiles import RolViewSet
+
+__all__ = [
+    # Auth views
+    'CustomTokenObtainPairView',
+    'RegistroView',
+    'PerfilView',
+    'cambiar_password_view',
+    'logout_view',
+    'verificar_token_view',
+    'ResetPasswordRequestView',
+    'ResetPasswordConfirmView',
+    # CRUD views
+    'UsuarioViewSet',
+    # Profiles views
+    'RolViewSet',
+]
