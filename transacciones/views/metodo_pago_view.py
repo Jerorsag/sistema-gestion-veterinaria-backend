7de2a6548dd71efbf0055b0baece138dfd858132
JeroRodriguez <jeronimoroseag@gmail.com>
2025-11-29 13:18:52 -0500

    feat(metodos-pago): Se agrega funcion para crear metodos de pago

--- transacciones/views/metodo_pago_view.py
+++ transacciones/views/metodo_pago_view.py
@@ -3,6 +3,13 @@ from transacciones.models.metodo_pago import MetodoPago
 from transacciones.serializers.metodo_pago_serializer import MetodoPagoSerializer
 
 
-class MetodoPagoListView(generics.ListAPIView):
+class MetodoPagoListView(generics.ListCreateAPIView):
+    """
+    Vista para listar y crear métodos de pago.
+    
+    Permite:
+    - GET: Listar todos los métodos de pago
+    - POST: Crear un nuevo método de pago
+    """
     queryset = MetodoPago.objects.all()
     serializer_class = MetodoPagoSerializer
